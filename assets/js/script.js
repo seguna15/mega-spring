842c3f6789d9c5152ee6c0e5552cf915400cea90
seguna15 <seguna15@gmail.com>
2022-06-26 09:27:24 +0100

    swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,34 @@ hideButton.addEventListener('click', () => {
         el.classList.toggle('hidden');
     })
 });
+
+/*
+* Swipper
+*/
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   
+    loop: true,
+
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    },
+});
